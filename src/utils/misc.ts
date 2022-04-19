00b38592295ba4b44cbfb47f8d8fb9e417bdbd54
uxname <uxname@gmail.com>
2022-04-19 18:05:10 +0300

    Fixed urls

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -5,9 +5,13 @@ export function redirect(url: string): void {
 }
 
 export function generateFacebookAuthUrl(): string {
-    return `https://www.facebook.com/v13.0/dialog/oauth?client_id=${process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_APP_URL}/auth/facebook-auth&state=state${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_APP_URL}/auth/google-auth&flowName=GeneralOAuthFlow`;
+    const url = `https://www.facebook.com/v13.0/dialog/oauth?client_id=${process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_APP_URL}/auth/facebook-auth&state=state`;
+    console.log({facebookAuthUrl: url});
+    return url;
 }
 
 export function generateGoogleAuthUrl(): string {
-    return `https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?access_type=offline&scope=https://www.googleapis.com/auth/userinfo.profile&response_type=code&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_APP_URL}/auth/google-auth&flowName=GeneralOAuthFlow`;
+    const url = `https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?access_type=offline&scope=https://www.googleapis.com/auth/userinfo.profile&response_type=code&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_APP_URL}/auth/google-auth&flowName=GeneralOAuthFlow`;
+    console.log({googleAuthUrl: url});
+    return url;
 }
