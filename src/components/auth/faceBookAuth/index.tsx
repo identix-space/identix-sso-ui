a56349209d7595fdabdb3a1dbe97bfebf1ae3a5a
odiinnn <msinkevic103@gmail.com>
2022-04-26 19:16:55 +0300

    add simple loader

--- src/components/auth/faceBookAuth/index.tsx
+++ src/components/auth/faceBookAuth/index.tsx
@@ -7,7 +7,7 @@ import {
     generateFacebookAuthUrl,
     redirect
 } from '../../../utils/misc';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 export const FacebookAuth = (props: { redirectUrl: string }) => {
 
@@ -38,7 +38,22 @@ export const FacebookAuth = (props: { redirectUrl: string }) => {
     }
 
     return (
-        <Loader>Processing....</Loader>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+            <Loader p={true}><span>P</span></Loader>
+            <Loader><span>r</span></Loader>
+            <Loader p={true}><span>o</span></Loader>
+            <Loader><span>c</span></Loader>
+            <Loader p={true}><span>e</span></Loader>
+            <Loader><span>s</span></Loader>
+            <Loader p={true}><span>s</span></Loader>
+            <Loader><span>i</span></Loader>
+            <Loader p={true}><span>n</span></Loader>
+            <Loader><span>g</span></Loader>
+            <Loader p={true}><span>.</span></Loader>
+            <Loader><span>.</span></Loader>
+            <Loader p={true}><span>.</span></Loader>
+            <Loader><span>.</span></Loader>
+        </div>
     );
 };
 
@@ -71,10 +86,44 @@ const Button = styled.button`
   }
 `;
 
-const Loader = styled.p`
+interface LoaderProps {
+    primary?: boolean;
+    p?: boolean;
+    r?: boolean;
+}
+
+const moveUp = keyframes`
+  from {
+    margin-top: 100%;
+    height: 20%;
+  }
+
+  40% {
+    margin-top: 0%;
+    height: 10%;
+  }
+
+  60% {
+    margin-top: 0%;
+    height: 10%;
+  }
+
+  to {
+    margin-top: 100%;
+    height: 20%;
+  }
+`;
+
+
+const Loader = styled.p<LoaderProps>`
   color: #FFFFFF;
+  animation: ${props => props.p ? moveUp : stay} ${props => props.p ? 4 : 0}s  linear infinite;
   font-size: 32px;
   font-weight: 700;
   margin-bottom: 80px;
+`;
+
+const stay = keyframes`
+  
 `;
 
--- src/components/auth/googleAuth/index.tsx
+++ src/components/auth/googleAuth/index.tsx
@@ -7,7 +7,7 @@ import {
     generateGoogleAuthUrl,
     redirect
 } from '../../../utils/misc';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 export const GoogleAuth = (props: { redirectUrl: string }) => {
     console.log('GoogleAuth props', props);
@@ -40,7 +40,23 @@ export const GoogleAuth = (props: { redirectUrl: string }) => {
     }
 
     return (
-        <Loader>Processing....</Loader>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+            <Loader p={true}><span>P</span></Loader>
+            <Loader><span>r</span></Loader>
+            <Loader p={true}><span>o</span></Loader>
+            <Loader><span>c</span></Loader>
+            <Loader p={true}><span>e</span></Loader>
+            <Loader><span>s</span></Loader>
+            <Loader p={true}><span>s</span></Loader>
+            <Loader><span>i</span></Loader>
+            <Loader p={true}><span>n</span></Loader>
+            <Loader><span>g</span></Loader>
+            <Loader p={true}><span>.</span></Loader>
+            <Loader><span>.</span></Loader>
+            <Loader p={true}><span>.</span></Loader>
+            <Loader><span>.</span></Loader>
+        </div>
+
     );
 };
 
@@ -73,9 +89,45 @@ const Button = styled.button`
   }
 `;
 
-const Loader = styled.p`
+interface LoaderProps {
+    primary?: boolean;
+    p?: boolean;
+    r?: boolean;
+}
+
+const moveUp = keyframes`
+  from {
+    margin-top: 100%;
+    height: 20%;
+  }
+
+  40% {
+    margin-top: 0%;
+    height: 10%;
+  }
+
+  60% {
+    margin-top: 0%;
+    height: 10%;
+  }
+
+  to {
+    margin-top: 100%;
+    height: 20%;
+  }
+`;
+
+
+const Loader = styled.p<LoaderProps>`
   color: #FFFFFF;
+  animation: ${props => props.p ? moveUp : stay} ${props => props.p ? 4 : 0}s  linear infinite;
   font-size: 32px;
   font-weight: 700;
   margin-bottom: 80px;
 `;
+
+const stay = keyframes`
+  
+`;
+
+
